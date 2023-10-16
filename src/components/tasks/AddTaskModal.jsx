4061f1037119d4cc6e4c96ced7d169f16473f7a1
abukawsar45 <kawsar1146236@gmail.com>
2023-10-16 15:55:23 +0600

    signup part done

--- src/components/tasks/AddTaskModal.jsx
+++ src/components/tasks/AddTaskModal.jsx
@@ -4,105 +4,102 @@ import { useDispatch } from 'react-redux';
 import { addTask } from '../../redux/features/tasks/tasksSlice';
 
 const AddTaskModal = ({ isOpen, setIsOpen }) => {
-  const { register, handleSubmit, reset } = useForm();
   const dispatch = useDispatch();
+
+  const { register, handleSubmit, reset } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+    dispatch(addTask(data));
+    onCancel();
+  };
 
   const onCancel = () => {
     reset();
     setIsOpen(false);
   };
 
-  const onSubmit = (data) => {
-    dispatch(addTask(data));
-    onCancel();
-  };
 
   return (
-    <Modal isOpen={isOpen} setIsOpen={setIsOpen} title="Programming Hero">
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="flex flex-col mb-5">
-          <label htmlFor="title" className="mb-2">
-            Title
-          </label>
+        <div className='flex flex-col gap-5'>
+          <label htmlFor='title'>Title</label>
           <input
-            className="w-full rounded-md"
-            type="text"
-            id="title"
+            className='w-full rounded-lg'
+            type='text'
             {...register('title')}
+            id=''
           />
         </div>
-        <div className="flex flex-col mb-5">
-          <label htmlFor="title" className="mb-2">
+
+        <div className='flex flex-col mb-5'>
+          <label htmlFor='description' className='mb-2'>
             Description
           </label>
           <textarea
-            className="w-full rounded-md"
-            type="text"
-            id="description"
+            id='description'
+            cols='5'
+            rows='3'
             {...register('description')}
           />
         </div>
-        <div className="flex flex-col mb-5">
-          <label htmlFor="title" className="mb-2">
+        <div className='flex flex-col mb-5'>
+          <label htmlFor='deadline' className='mb-2'>
             Deadline
           </label>
+          {/* <input type='date' defaultValue={new Date()} id='date' {...register('date')} /> */}
           <input
-            className="w-full rounded-md"
-            type="date"
-            id="date"
+            type='date'
+            defaultValue={new Date().toISOString().substr(0, 10)}
+            id='date'
             {...register('date')}
           />
         </div>
-        <div className="flex flex-col mb-5">
-          <label htmlFor="title" className="mb-2">
+        <div className='flex flex-col mb-5'>
+          <label htmlFor='Author' className='mb-2'>
             Assign to
           </label>
           <select
-            className="w-full rounded-md"
-            id="assignedTo"
+            className='w-full rounded-lg'
+            id='assignedTo'
             {...register('assignedTo')}
           >
-            <option value="Mir Hussain">Mir Hussain</option>
-            <option value="Mezba Abedin">Mezba Abedin</option>
-            <option value="Nahid Hasan">Nahid Hasan</option>
-            <option value="Mizanur Rahman">Mizanur Rahman</option>
-            <option value="Tanmoy Parvez">Tanmoy Parvez</option>
-            <option value="Fahim Ahmed Firoz">Fahim Ahmed Firoz</option>
-            <option value="Rahatul Islam">Rahatul Islam</option>
-            <option value="Samin Israr Ravi">Samin Israr Ravi</option>
-            <option value="Mehedi Anik">Mehedi Anik</option>
-            <option value="Ehtisam Haq">Ehtisam Haq</option>
-            <option value="Anisur Rahman">Anisur Rahman</option>
-            <option value="Muktadir Hasan">Muktadir Hasan</option>
-            <option value="Masud Alam">Masud Alam</option>
+            <option defaultValue='Abu Kaw Sar'>Abu Kaw Sar</option>
+            <option value='Rasel'>Rasel</option>
+            <option value='Mostofa'>Mostofa</option>
+            <option value='Ibrahim'>Ibrahim</option>
+            <option value='Shohel'>Shohel</option>
+            <option value='Shohid'>Shohid</option>
+            <option value='Kayes'>Kayes</option>
+            <option value='Imran'>Imran</option>
+            <option value='Ershad'>Ershad</option>
           </select>
         </div>
-        <div className="flex flex-col mb-5">
-          <label htmlFor="title" className="mb-2">
+        <div className='flex flex-col mb-5'>
+          <label htmlFor='title' className='mb-2'>
             Priority
           </label>
           <select
-            className="w-full rounded-md"
-            id="priority"
+            className='w-full rounded-lg'
+            id='priority'
             {...register('priority')}
           >
-            <option defaultValue value="high">
-              High
-            </option>
-            <option value="medium">Medium</option>
-            <option value="low">Low</option>
+            <option defaultValue='high'>High</option>
+            <option value='mdedium'>Medium</option>
+            <option value='low'>Low</option>
           </select>
         </div>
-        <div className="flex gap-3 justify-end">
+        <div className='flex gap-3 justify-end'>
           <button
             onClick={() => onCancel()}
-            type="button"
-            className="btn btn-danger "
+            type='button'
+            className='btn btn-danger'
           >
             Cancel
           </button>
-          <button type="submit" className="btn btn-primary ">
-            submit
+          <button type='submit' className='btn btn-primary'>
+            Add
           </button>
         </div>
       </form>
--- src/redux/features/user/userSlice.js
+++ src/redux/features/user/userSlice.js
@@ -1,14 +1,54 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+import auth from './../../../utils/firebase.config';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 
 const initialState = {
-  name: 'Mir Hussain',
-  email: 'mir@gmail.com',
+  name: '',
+  email: '',
+  isLoading: true,
+  isError: false,
+  error: '',
 };
 
+export const createUser = createAsyncThunk(
+  'userSlice/createUser',
+  async ({ email, password }) => {
+    const data = await createUserWithEmailAndPassword (auth, email, password);
+    console.log(data);
+    return {
+      email: data.user.email,
+      name: data.user.displayName,
+    };
+  }
+);
 const userSlice = createSlice({
   name: 'userSlice',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(createUser.pending, (state) => {
+      state.isLoading = true;
+      state.isError = false;
+      state.email = '';
+      state.name = '';
+      state.error = '';
+    });
+    builder.addCase(createUser.fulfilled, (state, { payload }) => {
+      state.isLoading = false;
+      state.isError = false;
+      state.email = payload.email;
+      state.name = payload.name;
+      state.error = '';
+    });
+    builder.addCase(createUser.rejected, (state, action) => {
+      state.isLoading = false;
+      state.isError = false;
+      state.email = '';
+      state.name = '';
+      state.error = action.error.message;
+    });
+  },
 });
 
 export default userSlice.reducer;
