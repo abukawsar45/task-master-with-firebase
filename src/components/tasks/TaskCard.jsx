ec98fafa1bbed15e01c8099462c9752434f9e5cf
abukawsar45 <kawsar1146236@gmail.com>
2023-10-22 00:22:44 +0600

    basic part done

--- src/components/tasks/TaskCard.jsx
+++ src/components/tasks/TaskCard.jsx
@@ -23,7 +23,7 @@ const TaskCard = ({ task }) => {
     updateTask(options);
   }
 
-  // console.log(data)
+  console.log(task)
   // console.log(error)
 
   let updatedStatus;
@@ -39,12 +39,12 @@ const TaskCard = ({ task }) => {
     <div className="bg-secondary/10 rounded-md p-5">
       <h1
         className={`text-lg font-semibold mb-3 ${
-          task.priority === 'high' ? 'text-red-500' : ' '
-        } ${task.priority === 'medium' ? 'text-yellow-500' : ' '} ${
-          task.priority === 'low' ? 'text-green-500' : ' '
+          task.priority === 'High' ? 'text-red-500' : ' '
+        } ${task.priority === 'Medium' ? 'text-yellow-500' : ' '} ${
+          task.priority === 'Low' ? 'text-green-500' : ' '
         }`}
       >
-        {task?.title}
+        {task?.title} ,adsa
       </h1>
       <p className="mb-3">{task?.description}</p>
       <p className="text-sm">Assigned to - {task?.assignedTo}</p>
