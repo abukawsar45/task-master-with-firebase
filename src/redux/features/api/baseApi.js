5a0748497011c4f00da5d9f7b50bab405da784ef
abukawsar45 <kawsar1146236@gmail.com>
2023-10-22 10:05:47 +0600

    update server url

--- src/redux/features/api/baseApi.js
+++ src/redux/features/api/baseApi.js
@@ -3,12 +3,11 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const baseApi = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://taskmaster-server-roan.vercel.app',
+    // baseUrl: 'https://taskmaster-server-roan.vercel.app',
+    baseUrl: 'https://task-master-server.up.railway.app/',
   }),
   tagTypes: ['Tasks'],
-  endpoints: () => ({
-    
-  }),
+  endpoints: () => ({}),
 });
 
 // addTask: builder.mutation({
